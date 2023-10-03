d83f0838a6ccf18c7f353b2910aa0bf53ee628b1
Sakti Sriraj Mishra <reply.saktisriraj@gmail.com>
2023-10-03 23:47:35 +0530

    Sidebar Added

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import {useState , useEffect} from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import { Videos, Sidebar } from './';
 
 const Feed = () => {
   return (
-    <div>
-      Feed
-    </div>
+    <Stack sx={{ flexDirection:{ sx:"column", md:"row"} }}>
+      
+      <Box sx={{ height:{sx:'auto', md:'92vh'}, borderRight:'1px solid #3d3d3d', px:{sx:0,md:2} }}>
+      
+        <Sidebar />
+      
+        <Typography className="copyright" variant="bpdy2" sx={{mt:1.5, color:'#fff'}}>
+          Copyright 2023 SaktiSriraj
+        </Typography>
+      
+      </Box>
+
+      <Box p={2} sx={{overflowY:'auto', height:'90vh', flex:2}}>
+        <Typography variant ="h4" fontWeight='bold' mb={2} sx={{color:'white'}}>
+          New <span style={{ color:'#FC1503'}}>Videos</span>
+        </Typography>
+      </Box>
+      
+      <Videos videos={[]} />
+    
+    </Stack>
   )
 }
 
